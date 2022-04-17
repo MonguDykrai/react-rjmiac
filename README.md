e6838f2e81c3e68495cfc85ad942cdfd88633e1b
MonguDykrai <kisaihou@gmail.com>
2022-04-17 14:11:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-rjmiac-xnkgel
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rjmiac-xnkgel)
---
+++ index.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { TimePicker, Button } from 'antd';
+import moment from 'moment';
+import _ from 'lodash';
+import { v4 as uuidv4 } from 'uuid';
+
+const App = () => {
+  const [values, setValues] = useState([]);
+
+  useEffect(() => {
+    setValues([
+      ...values,
+      {
+        id: uuidv4(),
+        moments: [moment('0000', 'HHmm'), moment('2359', 'HHmm')],
+        disabledTime: (
+          // currentTime
+          now,
+          type
+        ) => {
+          console.log(now, type);
+          console.log(now.format('HHmm'), type);
+          return {
+            disabledHours: () => {
+              return [];
+            },
+            disabledMinutes: (selectedHour) => {
+              // console.log(selectedHour);
+              return [];
+            },
+          };
+        },
+      },
+    ]);
+  }, []);
+  return (
+    <>
+      {values.map((value) => {
+        const { id, moments, disabledTime } = value;
+        return (
+          <div key={id}>
+            <TimePicker.RangePicker
+              format="HHmm"
+              onChange={(changeValue) => {
+                const valuesCopy = _.cloneDeep(values);
+                changeValue = {
+                  ...changeValue,
+                  id,
+                  moments: changeValue.map((v) => moment(v)),
+                };
+                const findIndex = valuesCopy.findIndex((vc) => vc.id === id);
+                valuesCopy[findIndex] = changeValue;
+                setValues(valuesCopy);
+              }}
+              value={moments}
+              disabledTime={disabledTime}
+            />
+          </div>
+        );
+      })}
+      <Button
+        onClick={() => {
+          setValues([
+            ...values,
+            {
+              id: uuidv4(),
+              value: [],
+            },
+          ]);
+        }}
+      >
+        新增
+      </Button>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('container'));
